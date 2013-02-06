d8e5bdcca71ada64f8aa6c14413907c9e38c70f4
Lee Quarella <leequarella@frog-dev-3.(none)>
2013-02-06 15:03:19 -0500

    volumes spec-lib

--- spec/unit_converter/volumes_spec.rb
+++ spec/unit_converter/volumes_spec.rb
@@ -1,39 +1,38 @@
 require "unit_converter"
+require "roundable_spec"
 
-describe UnitConverter do
-
-
+describe UnitConverter::Volumes do
+  it_behaves_like "roundable"
   context "liters to gallons" do
     it "without explicit precision, default to 2 decimals" do
-      gallons = UnitConverter.liters_to_gallons(10)
+      gallons = UnitConverter::Volumes.liters_to_gallons(10)
       gallons.should eq 2.64
-      gallons = UnitConverter.liters_to_gallons(12)
+      gallons = UnitConverter::Volumes.liters_to_gallons(12)
       gallons.should eq 3.17
     end
 
     it "converts with explicit precision" do
-      gallons = UnitConverter.liters_to_gallons(10, 3)
+      gallons = UnitConverter::Volumes.liters_to_gallons(10, 3)
       gallons.should eq 2.642
-      gallons = UnitConverter.liters_to_gallons(12, 3)
+      gallons = UnitConverter::Volumes.liters_to_gallons(12, 3)
       gallons.should eq 3.17
     end
   end
 
  context "gallons to liters" do
     it "without explicit precision, default to 2 decimals" do
-      liters = UnitConverter.gallons_to_liters(10)
+      liters = UnitConverter::Volumes.gallons_to_liters(10)
       liters.should eq 37.85
-      liters = UnitConverter.gallons_to_liters(12)
+      liters = UnitConverter::Volumes.gallons_to_liters(12)
       liters.should eq 45.42
     end
 
     it "converts with explicit precision" do
-      liters = UnitConverter.gallons_to_liters(10, 3)
+      liters = UnitConverter::Volumes.gallons_to_liters(10, 3)
       liters.should eq 37.854
-      liters = UnitConverter.gallons_to_liters(12, 3)
+      liters = UnitConverter::Volumes.gallons_to_liters(12, 3)
       liters.should eq 45.425
     end
   end
-
-
 end
+
