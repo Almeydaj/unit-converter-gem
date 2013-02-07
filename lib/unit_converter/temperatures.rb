91e8450e10337d0d2af5994ac0d2a74a94065ab4
Nash <nash@ubuntu.(none)>
2013-02-07 15:36:46 -0500

    unit converter for temperatures spec & lib

---
+++ lib/unit_converter/temperatures.rb
@@ -0,0 +1,36 @@
+module UnitConverter
+  class Temperatures
+    include UnitConverter::Math
+
+    def self.kelvin_to_celsius(kelvin, precision=2)
+      exact = kelvin - 273.15
+      round_to_precision(exact, precision)
+    end
+
+    def self.celsius_to_kelvin(celsius, precision=2)
+      exact = celsius + 273.15
+      round_to_precision(exact, precision)
+    end
+
+    def self.kelvin_to_fahrenheit(kelvin, precision=2)
+      exact = (kelvin * 9.0 / 5.0) - 459.67
+      round_to_precision(exact, precision)
+    end
+
+    def self.fahrenheit_to_kelvin(fahrenheit, precision=2)
+      exact = (fahrenheit + 459.67) * 5.0 / 9.0
+      round_to_precision(exact, precision)
+    end
+
+    def self.fahrenheit_to_celsius(fahrenheit, precision=2)
+      exact = (fahrenheit - 32) * 5.0 / 9.0
+      round_to_precision(exact, precision)
+    end
+
+    def self.celsius_to_fahrenheit(celsius, precision=2)
+      exact = (celsius * 9.0 / 5.0) + 32
+      round_to_precision(exact, precision)
+    end
+
+  end
+end
